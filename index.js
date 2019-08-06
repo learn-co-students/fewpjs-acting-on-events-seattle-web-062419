f608f6570f8fbf88b956cebe47c932f10b92816f
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-08-06 16:56:26 -0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.left;
+console.log(dodger.style.left)
+dodger.style.bottom;
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+document.addEventListener("keydown",function(event) {
+    if(event.key === "ArrowLeft") {
+        const leftNumbers = dodger.style.left.replace("px","");
+        console.log(leftNumbers)
+        const left = parseInt(leftNumbers,10);
+        if (left > 0) {
+            dodger.style.left = `${left - 1}px`;
+          }
+    }
+})

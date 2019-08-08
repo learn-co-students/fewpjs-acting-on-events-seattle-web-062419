39651aa2cdb6a2b33329d4d7862c4ae0f47a8ae1
github username <github email address>
2019-08-08 13:30:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+// capturing element 'dodger'
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+function moveDodgerRight() {
+    var rightNumbers = dodger.style.left.replace("px", "");
+    var right = parseInt(rightNumbers, 10);
+
+    if (right > 0) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
